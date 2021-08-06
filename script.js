41ae7f243e83df3cf7924fa381f58571e3d419b4
cvncerboy <74409676+cvncerboy@users.noreply.github.com>
2021-08-06 16:50:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let burger = document.querySelector('.burger');
+let unordered = document.querySelector('.navigasi');
+
+burger.addEventListener('click', () => {
+    unordered.classList.toggle('slide');
+});
+
+
